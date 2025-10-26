8b9bfd07c63c6eb7d8190103d65061c7b7dbb23b
Viwu <sinconsistencia@gmail.com>
2025-10-25 21:10:12 -0500

    config/version is an auto-file

--- source/sphis/onescrine/InitState.hx
+++ source/sphis/onescrine/InitState.hx
@@ -1,11 +1,15 @@
 package sphis.onescrine;
 
 import flixel.FlxState;
+import sphis.onescrine.textvar.Textvar;
 
 class InitState extends FlxState
 {
 	override public function create()
 	{
+		Textvar.file_names.push('config/version');
+		Textvar.parseAllFiles(true);
+
 		super.create();
 	}
 
