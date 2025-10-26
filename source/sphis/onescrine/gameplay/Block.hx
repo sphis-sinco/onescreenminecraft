ea26b5eebc766e9762fafa1aae1c75858b73ceb7
Viwu <sinconsistencia@gmail.com>
2025-10-25 21:51:49 -0500

    components

--- source/sphis/onescrine/gameplay/Block.hx
+++ source/sphis/onescrine/gameplay/Block.hx
@@ -1,11 +1,16 @@
 package sphis.onescrine.gameplay;
 
+import sphis.onescrine.gameplay.components.Component;
+
 class Block
 {
 	public var id:String;
 
-	public function new(id:String)
+	public var components:Array<Component> = [];
+
+	public function new(id:String, ?components:Array<Component>)
 	{
 		this.id = id;
+		this.components = components;
 	}
 }
---
+++ source/sphis/onescrine/gameplay/components/Component.hx
@@ -0,0 +1,14 @@
+package sphis.onescrine.gameplay.components;
+
+class Component
+{
+	public var tag:String;
+
+	public var data:Dynamic;
+
+	public function new(tag:String, data:Dynamic)
+	{
+		this.tag = tag;
+		this.data = data;
+	}
+}
