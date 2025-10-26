da27a2d77fddc91a7ac042bf5a305e856c0a2418
Viwu <sinconsistencia@gmail.com>
2025-10-25 20:39:21 -0500

    constants file

---
+++ source/sphis/onescrine/Constants.hx
@@ -0,0 +1,9 @@
+package sphis.onescrine;
+
+import sphis.any.Version;
+
+class Constants
+{
+    public static var TITLE:String = "Onescrine";
+	public static var VERSION:Version = new Version(0, 1);
+}
