3865831bf839aa486d4651147d498c8a6393d7f7
Viwu <sinconsistencia@gmail.com>
2025-10-26 12:46:45 -0500

    dumy blocks

--- source/sphis/onescrine/gameplay/World.hx
+++ source/sphis/onescrine/gameplay/World.hx
@@ -7,13 +7,14 @@ class World
 	public static var LAYERS(get, never):Int;
 
 	static function get_LAYERS():Int
-	{
 		return Std.int(Textvar.getVariable('world_layers') ?? 4);
-	}
 
 	public static function getLayerBlock(layer:Int):String
 		return Textvar.getVariable('world_layer_' + Std.string(layer) + '_block') ?? 'temp-block';
 
 	public static function getLayerHeight(layer:Int):Int
 		return Textvar.getVariable('world_layer_' + Std.string(layer) + '_y') ?? 0;
+
+	public static function getLayerRandomHeight(layer:Int):Int
+		return Textvar.getVariable('world_layer_' + Std.string(layer) + '_y_random_max_amount') ?? 0;
 }
