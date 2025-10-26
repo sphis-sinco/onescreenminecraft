6ccd14233313f717145c6dd7c06ce3b94a94a031
Viwu <sinconsistencia@gmail.com>
2025-10-26 13:42:45 -0500

    world_layer_3_y_random_max_amount

--- source/sphis/onescrine/gameplay/PlayState.hx
+++ source/sphis/onescrine/gameplay/PlayState.hx
@@ -44,19 +44,35 @@ class PlayState extends FlxState
 			if (current_layer_height > y && layer == 0)
 				y = current_layer_height;
 
+			var next_layer_block = layerBlocks[layer + 1] ?? '';
 			var next_layer_height:Null<Int> = layerHeights[layer + 1] ?? null;
 			var next_layer_random_height:Null<Int> = layerRandomHeights[layer + 1] ?? null;
 
-			trace(current_layer_block);
-			trace(next_layer_height);
-			trace(next_layer_random_height);
-			trace(y);
+			trace('current_layer_block: ' + current_layer_block);
+			trace('current_layer_height: ' + current_layer_height);
+
+			trace('next_layer_block: ' + next_layer_block);
+			trace('next_layer_height: ' + next_layer_height);
+			trace('next_layer_random_height: ' + next_layer_random_height);
+
+			trace('y: ' + y);
 
 			while (x < 20)
 			{
-				if (current_layer_block != null)
+				if (current_layer_block != null && current_layer_block != '')
 				{
-					var newBlock:Block = new Block(current_layer_block);
+					var newBlock:Block;
+					var is_next_block:Bool = false;
+
+					if ((next_layer_block != null && next_layer_block != '')
+						&& (FlxG.random.bool(FlxG.random.float(0, 100)) && (next_layer_height - y <= next_layer_random_height)))
+					{
+						newBlock = new Block(next_layer_block);
+						is_next_block = true;
+					}
+					else
+						newBlock = new Block(current_layer_block);
+
 					newBlock.x = x * (newBlock.width);
 					newBlock.y = y * (newBlock.height);
 					blocks.add(newBlock);
@@ -69,7 +85,7 @@ class PlayState extends FlxState
 			x = 0;
 
 			if (next_layer_height != null)
-				if (y >= next_layer_height)
+				if (y == next_layer_height)
 					layer++;
 		}
 
